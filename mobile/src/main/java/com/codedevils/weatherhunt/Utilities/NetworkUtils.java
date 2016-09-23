4f71917d970235c0d66897c9281eb650ec5b56e1
nithesh1990 <nithesh1990@gmail.com>
2016-09-23 00:10:49 -0700

    1. Added Gson and Volley dependencies 2. Added base urls and paths in network utils

---
+++ mobile/src/main/java/com/codedevils/weatherhunt/Utilities/NetworkUtils.java
@@ -0,0 +1,41 @@
+package com.codedevils.weatherhunt.Utilities;
+
+/**
+ * Created by "nithesh" on 9/22/2016.
+ */
+
+public class NetworkUtils {
+
+    public static final String BASE_URL = "dataservice.accuweather.com";
+
+    public static final String API_KEY = "aY2QV83X0YVzOCenncSAXUlUSUPGmFqJ";
+
+    public static final String API_VERSION = "v1";
+
+    //Location API
+    public static final String PATH_LOCATIONS = "locations";
+    public static final String PATH_LOC_CITIES = "cities";
+    public static final String PATH_AUTOCOMPLETE = "autocomplete";
+    public static final String PATH_GEOPOSITION_SEARCH = "geoposition/search";
+
+    //Forecasts API
+    public static final String PATH_FORECASTS = "forecasts";
+    public static final String PATH_DAILY = "daily";
+    public static final String PATH_1DAY = "1day";
+    public static final String PATH_5DAY = "5day";
+
+    public static final String PATH_HOURLY = "hourly";
+    public static final String PATH_1HOUR = "1hour";
+    public static final String PATH_12HOUR = "12hour";
+
+    //Current Conditions API
+    public static final String PATH_CURRENT_CONDITIONS = "currentconditions";
+
+    //Indices API
+    public static final String PATH_INDICES = "indices";
+
+
+    //Weather Alarm API
+    public static final String PATH_WEATHER_ALARMS = "alarms";
+
+}
